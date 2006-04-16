f24f1252d32f2d00dee4644c1d8aac2bd4a04532
Marc <sanmarcos@users.noreply.github.com>
2006-04-16 22:00:26 +0000

    Cleaned, TODO

--- globals.py
+++ globals.py
@@ -4,53 +4,55 @@
 # Global variables
 import os, sys
 
-# Set up global itaka variables
+#: Version (do not change)
 version = "Devel"
-# See what system we are running on posix/nt
+#: Check system or specify per os.name standard
 system = os.name
-# Where itaka images are stored...
+#: Itaka images/ directory
 image_dir = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "images/")
 
-# Method (ftp, server)
+#: Method (ftp, server)
 method = "server"
 
 # FTP Variables
+#: FTP host
 ftphost = "ftp.usuarios.lycos.es"
+#: FTP port
 ftport = 21
+#: FTP username
 ftpuser = "cgarcia2003"
+#: FTP password
 ftpass = "pass"
-# Up dir (Default: blank)
+#: FTP directory (default: blank)
 ftpdir = "/itaka"
-# Interval between screenshots
+#: FTP Interval between uploads
 ftptime = 8
 # Debug level (From ftplib.set_debuglevel) 0-2
 ftpdebug = 0
 
-# TCP port
+#: Server TCP port
 port = 8000
-# Path: where to save the screenshot.
+#: Save path for screenshot.
 path = '/tmp'
 if (system != 'posix'): 
 	path = os.environ.get('TEMP') or os.environ.get('TMP') or os.getcwd()
 
-# Image format: 'jpeg' or 'png'
+#: Screenshot format (jpeg, png)
 format = 'jpeg'
-# Image quality: int 0 to 100.
+#: ScreScreenshott quality (0-100)
 quality = 70
-# Alert on request (bell)
+#: Alert on request (Bell)
 alert = True
-# Audio Player support
+#: Audio Player support
 audio = False
-# Libnotify support
+#: GUI notification support
 notify = False
-if (os.name != "nt" ):
-	notify = True
 
 # Customization of HTML
 if (audio): 
-	# This is the Audio HTML.
+	#: Audio HTML of Server
 	audiohtml = '<iframe src="audio" width="100%" height="30" style="border: 0;" border="0">Your browser does not support IFRAME. <a href="audio">Click herek</a></iframe><br />'
 else:	audiohtml = '' 
 
-# This is the HTML that will be displayed
+#: HTML of Server
 html = '<html><body> ' + audiohtml  + ' <img src="screenshot" alt="If you are seeing this message it means there was an error in Itaka or you are using a text-only browser." border="0"></a></body</html>'
