6c3c7b39cbd241fe17d56f3bb2a0f32883a2f960
Marc <sanmarcos@users.noreply.github.com>
2007-06-06 05:39:18 +0000

    Merged back newui branch
    git-svn-id: https://localhost/svn/itaka/trunk@124 f799c825-8911-0410-abab-818b245388f9

--- console.py
+++ console.py
@@ -1,63 +1,70 @@
 #! /usr/bin/env python
 # -*- coding: utf8 -*-
 #
-# This program is free software; you can redistribute it and/or modify
+# Itaka is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# any later version.
 # 
-# This program is distributed in the hope that it will be useful,
+# Itaka is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
+# along with Itaka; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # Copyright 2003-2007 Marc E. <santusmarc_at_gmail.com>.
 # http://itaka.jardinpresente.com.ar
+#
 # $Id$
 
-""" Itaka console handling engine """
-
-import config as iconfig
+""" Itaka console output and logging engine """
 
 class Console:
-    """ Console I/O handler organized by message type. Also handle GUI logging when passed an instance. """
+    """ Console I/O handler organized by message type. Also handle GUI logging when passed an instance """
 
-    def __init__(self, ginstance=False):
-        """ Init console handler with a GUI instance. """
-        if ginstance: self.igui = ginstance
-        print "[*] Itaka %s starting up..." % (iconfig.version)
+    def __init__(self, itakaglobals):
+        """ Initiate console handler with configuration globals """
+        self.itakaglobals = itakaglobals
+        if self.itakaglobals.output['normal']: print "[*] Itaka %s starting..." % (itakaglobals.version)
 
     def __del__(self):
-        """ Destructor. """
-        print "[*] Itaka shutting down..."
+        """ Quitting message """
+        if self.itakaglobals.output['normal']: print "[*] Itaka shutting down..."
 
-    def msg(self, message, gui=False):
-        """ Message handler. """
-        # The gui argument is for the FTP method. It couples the console logger with the GUI logger.
-        # In the Twisted method, logging is done by its engine.
-        # Note the peculiar syntax of the argument you must pass to logger.
-        # A dict with the first key being 'message', coupled with a str()'ed tuple'd message.
-        print "[*] %s" % (message)
-        if gui: self.igui.logger({'message': [str(message)]})
+    def msg(self, message, gui=False, eventsonly=True, icon=None):
+        """ Message handler. 'gui' is an instance of the 'Gui' class for logging purposes. 'eventsonly' is a boolean to spcecify if the log message will only go to the events log. 'icon' is gtk.STOCK_ICON string for the Gui event log """
+        if self.itakaglobals.output['normal']: 
+            print "[*] %s" % (message)
 
-    def warn(self, caller, message, gui=False):
-        """ Warning handler. """
+        # Twisted takes a dict with the first key being 'message', coupled with a str()'ed tuple'd message.
+        if gui: 
+            gui.logger({'message': [str(message)]}, False, None, eventsonly, icon)
+
+    def warn(self, caller, message, gui=False, eventsonly=False, icon=None):
+        """ Warning handler. 'caller' is a list or tuple specifying the class and method were the event ocurred. Arguments are inherited from msg() """
         self.array = ".".join(caller)
-        print "[*] WARNING: %s: %s" % (self.array, message)
-        if gui: self.igui.logger({'message': [str("[*] ERROR: %s: %s" % (self.array, message))]})		
+        if self.itakaglobals.output['normal']: 
+            print "[*] WARNING: %s: %s" % (self.array, message)
+        if gui: 
+            gui.logger({'message': [str(message), str("WARNING: %s: %s" % (self.array, message))]}, True, 'warning', eventsonly, icon)		
 
-    def debug(self, caller, message, gui=False):
-        """ Debug handler. """
+    def error(self, caller, message, gui=False, eventsonly=False, icon=None):
+        """ Error handler. 'caller' is a list or tuple specifying the class and method were the event ocurred. Arguments are inherited from msg() """
         self.array = ".".join(caller)
-        print "[*] DEBUG: %s: %s" % (self.array, message)
-        if gui: self.igui.logger({'message': [str("[*] ERROR: %s: %s" % (self.array, message))]})
+        if not self.itakaglobals.output['quiet']:
+            print "[*] ERROR: %s: %s" % (self.array, message)
+        if gui:
+            gui.expander.set_expanded(True)
+            gui.expander.set_sensitive(True)
+            gui.logger({'message': [str(message), str("ERROR: %s: %s" % (self.array, message))]}, True, 'error', eventsonly, icon)
 
-    def error(self, caller, message, gui=False):
-        """ Error handler. """
+    def debug(self, caller, message, gui=False, eventsonly=False, icon=None):
+        """ Debug handler. 'caller' is a list or tuple specifying the class and method were the event ocurred. Arguments are inherited from msg() """
         self.array = ".".join(caller)
-        print "[*] ERROR: %s: %s" % (self.array, message)
-        if gui: self.igui.logger({'message': [str("[*] ERROR: %s: %s" % (self.array, message))]})
+        if self.itakaglobals.output['debug']: 
+            print "[*] DEBUG: %s: %s" % (self.array, message)
+        if gui: 
+            gui.logger({'message': [str(message), str("DEBUG: %s: %s" % (self.array, message))]}, True, 'debug', eventsonly, icon)
--- screenshot.py
+++ screenshot.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python
 # -*- coding: utf8 -*-
 #
-# This program is free software; you can redistribute it and/or modify
+# Itaka is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# any later version.
 # 
-# This program is distributed in the hope that it will be useful,
+# Itaka is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
+# along with Itaka; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # Copyright 2003-2007 Marc E. <santusmarc_at_gmail.com>.
@@ -22,23 +22,16 @@
 
 """ Itaka screenshot engine """
 
-import gc, os
-
-# Itaka core modules
-import config as iconfig
-config = iconfig
-iconfig = iconfig.values
-
-import pygtk
+import gc, os, gtk, pygtk
 pygtk.require("2.0")
-import gtk 
 
-# Final absolute path to the screenshot file
-shotFile = os.path.join(iconfig['screenshot']['path'], 'itakashot.%s' % (iconfig['screenshot']['format']))
 
-def Screenshot():
-    """ Returns a screenshot file. """
-    # GTK Screenshooting code.
+def Screenshot(guiinstance, configuration):
+    """ Returns a screenshot file. 'configuration' is configuration values """
+
+    # Final absolute path to the screenshot file
+    shotFile = os.path.join(configuration['screenshot']['path'], 'itakashot.%s' % (configuration['screenshot']['format']))
+    
     w = gtk.gdk.screen_width()
     h = gtk.gdk.screen_height()
     screenshot = gtk.gdk.Pixbuf.get_from_drawable(
@@ -48,10 +41,13 @@ def Screenshot():
             0, 0, 0, 0, w, h)
 
     # Save the screnshot, checking before if to set JPEG quality
-    if iconfig['screenshot']['format'] == 'jpeg':
-        screenshot.save(shotFile, iconfig['screenshot']['format'].lower(), {"quality":str(iconfig['screenshot']['quality'])})
-    else:
-        screenshot.save(shotFile, iconfig['screenshot']['format'].lower())
+    try:
+        if configuration['screenshot']['format'] == 'jpeg':
+            screenshot.save(shotFile, configuration['screenshot']['format'].lower(), {"quality":str(configuration['screenshot']['quality'])})
+        else:
+            screenshot.save(shotFile, configuration['screenshot']['format'].lower())
+    except:
+        self.console.error(['ImageResource','screenshot'], "Could not save screenshot", guiinstance)
 
     # Important workaround to avoid a memory leak.
     # http://www.async.com.br/faq/pygtk/index.py?req=show&file=faq08.004.htp
